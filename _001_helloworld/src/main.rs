0bc2b7c16fc81f9e1b3133d94250fe548f8a3a44
mimaraslan <mimaraslan@gmail.com>
2021-11-15 21:37:50 +0300

    Hello Rust
    Hello Rust

--- _001_helloworld/src/main.rs
+++ _001_helloworld/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+
+    //this is single line comment
+
+    /* This is a
+       Multi-line comment
+    */
+
+    println!(); // newline
+    println!("Hello Rust");//Hello Rust
+    println!("Name: {}", "Mimar"); //Name: Mimar
+    println!("Name: {} \tSurname: {}", "Mimar", "Aslan"); //Name: Mimar 	Surname: Aslan
 }
